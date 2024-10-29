98d641347cdae7eef2ba040a2dcd648b70b20b10
danialvfd <danialvfd@gmail.com>
2024-10-29 08:43:56 +0400

    fix: operator problems

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -1,50 +1,55 @@
 const mainDisplay = document.getElementById("display");
 const history = document.getElementById("historyContent");
-var isCalculated = false;   // مشکل پاک نشدن const 
+var isCalculated = false;
+var lastInput = '';
 
-function _appendNumber(input) {
-    _setMainDisplayValue(input);
-    _setDefualtDisplayValue();
-}
-
-function _setMainDisplayValue(input) {
+function appendNumber(input) {
     if (isCalculated) {
-        _clearDisplay();
         isCalculated = false;
         mainDisplay.value = input;
     } else {
         mainDisplay.value += input;
+        lastInput = 'number';
     }
 }
 
-function _setDefualtDisplayValue() {
+function setDefualtDisplayValue() {
     if (mainDisplay.value === "0") {
         mainDisplay.value = "";
     }
 }
 
-function _appendOperator(input) {
-    if (input === 'sqrt') {
-        mainDisplay.value = Math.sqrt(eval(mainDisplay.value));
-    } else if (input === 'cos') {
-        const angle = eval(mainDisplay.value);
-        mainDisplay.value = Math.cos(angle);
-    } else if (input === 'pm') {
-        mainDisplay.value = -eval(mainDisplay.value);
-    } else if (input === 'Backspace') {
-        mainDisplay.value = mainDisplay.value.slice(0, -1);
-    } else {
-        mainDisplay.value += input;
+function appendOperator(input) {
+    if (lastInput === 'number') {
+        if (input === 'sqrt') {
+            mainDisplay.value = Math.sqrt(eval(mainDisplay.value));
+            lastInput = 'number';
+        } else if (input === 'cos') {
+            const angle = eval(mainDisplay.value);
+            mainDisplay.value = Math.cos(angle);
+            lastInput = 'number';
+        } else if (input === 'pm') {
+            mainDisplay.value = -eval(mainDisplay.value);
+            lastInput = 'number';
+        } else if (input === 'Backspace') {
+            mainDisplay.value = mainDisplay.value.slice(0, -1);
+        } else {
+            mainDisplay.value += input;
+            lastInput = 'operator';
+        }
     }
 }
 
-function _clearDisplay() {
-    mainDisplay.value = "0";
+function clearDisplay() {
+    mainDisplay.value = '0';
+    history.innerHTML = '';
+    lastInput = '';
 }
 
-function _calculate() {
+function calculate() {
     try {
         let result = eval(mainDisplay.value);    // const نباشد
+        result = parseFloat(result.toFixed(4));  // بدون parsfloat کلا 4 رقم اعشار می زند
         history.innerHTML = `${mainDisplay.value} = ${result}`;
         mainDisplay.value = result;
     }
@@ -53,24 +58,25 @@ function _calculate() {
     }
     finally {
         isCalculated = true;
+        lastInput = 'number';
     }
 }
 
 document.addEventListener('keydown', function (event) {
     if (!isNaN(event.key)) {
-        _appendNumber(event.key);
+        appendNumber(event.key);
     } else if (event.key === '+') {
-        _appendOperator('+');
+        appendOperator('+');
     } else if (event.key === '-') {
-        _appendOperator('-');
+        appendOperator('-');
     } else if (event.key === '*') {
-        _appendOperator('*');
+        appendOperator('*');
     } else if (event.key === '/') {
-        _appendOperator('/');
+        appendOperator('/');
     } else if (event.key === 'Enter') {
-        _calculate();
+        calculate();
     } else if (event.key === 'Backspace') {
-        _appendOperator('Backspace');
+        appendOperator('Backspace');
     }
 });
 
